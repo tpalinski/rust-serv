8c7ca2723a8b2ac498dd82f07fda027bf07e5ecf
tpalinski <tpalinski@icloud.com>
2023-04-15 23:53:22 +0200

    Add route parsing, refactor response string

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
 use std::str;
+mod route_parser;
 
 fn handle_request(stream: &mut TcpStream){
     println!("Client request stuff");
     let mut buf = [0; 1024];
     stream.read(&mut buf).expect("Error reading TcpStream");
-    let buf_text = match String::from_utf8(buf.to_vec()) {
-        Ok(v) => v,
-        Err(e) => panic!("Buffer error"),
-    };
-    println!("{}", buf_text);
-    let response_string = "HTTP/1.1 200 OK\r\n
-        Content-Type: text/plain\r\n
-        Custom-Header: nice\r\n
-        \r\n"; 
+    let response_string = "HTTP/1.1 200 OK\r
+Content-Type: text/plain\r
+Custom-Header: nice\r
+\r\n"; 
     stream.write(response_string.as_bytes());
     let content_string = "Test text";
     stream.write(content_string.as_bytes());
