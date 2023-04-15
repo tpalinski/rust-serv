1bab425e1517560d0b09b1827a3833bddcced704
tpalinski <tpalinski@icloud.com>
2023-04-15 21:37:10 +0200

    Add basic socket server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn handle_request(stream: TcpStream){
+    println!("Client request stuff");
+}
+
+
+fn main() -> std::io::Result<()> {
+    println!("Starting server");
+    let listener = TcpListener::bind("localhost:8080")?;
+
+    for stream in listener.incoming() {
+        handle_request(stream?);
+    }
+
+    Ok(())
 }
