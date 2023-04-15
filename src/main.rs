d20ad08e43c40fc0c616093f18a84217d46b85aa
tpalinski <tpalinski@icloud.com>
2023-04-16 01:08:40 +0200

    Handle custom routes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
 use std::str;
 
+use crate::router::Router;
+
 mod route_parser;
 mod router;
 
-fn handle_request(stream: &mut TcpStream){
-    println!("Client request stuff");
+fn handle_request(stream: &mut TcpStream, router: &mut Router){
     let mut buf = [0; 1024];
     stream.read(&mut buf).expect("Error reading TcpStream");
     let route = route_parser::parse_route(&buf);
     println!("{route}");
-    let response_string = "HTTP/1.1 200 OK\r
-Content-Type: text/plain\r
-Custom-Header: nice\r
-\r\n"; 
+    let (response_string, content_string) = router.handle_route(route);
     stream.write(response_string.as_bytes());
-    let content_string = "Test text";
     stream.write(content_string.as_bytes());
 }
 
@@ -23,9 +20,9 @@ Custom-Header: nice\r
 fn main() -> std::io::Result<()> {
     println!("Starting server");
     let listener = TcpListener::bind("localhost:8080")?;
-
+    let mut router = Router::default();
     for stream in listener.incoming() {
-        handle_request(&mut stream?);
+        handle_request(&mut stream?, &mut router);
     }
 
     Ok(())
