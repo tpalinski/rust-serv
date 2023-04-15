b2cfc59b2bfd79f07a415ab18740f8f0b0b308e0
tpalinski <tpalinski@icloud.com>
2023-04-15 23:25:00 +0200

    Add basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
-use std::net::{TcpListener, TcpStream};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+use std::str;
 
-fn handle_request(stream: TcpStream){
+fn handle_request(stream: &mut TcpStream){
     println!("Client request stuff");
+    let mut buf = [0; 1024];
+    stream.read(&mut buf).expect("Error reading TcpStream");
+    let buf_text = match String::from_utf8(buf.to_vec()) {
+        Ok(v) => v,
+        Err(e) => panic!("Buffer error"),
+    };
+    println!("{}", buf_text);
+    let response_string = "HTTP/1.1 200 OK\r\n
+        Content-Type: text/plain\r\n
+        Custom-Header: nice\r\n
+        \r\n"; 
+    stream.write(response_string.as_bytes());
+    let content_string = "Test text";
+    stream.write(content_string.as_bytes());
 }
 
 
@@ -10,7 +25,7 @@ fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("localhost:8080")?;
 
     for stream in listener.incoming() {
-        handle_request(stream?);
+        handle_request(&mut stream?);
     }
 
     Ok(())
