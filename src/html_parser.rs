e07f144cd979d385e1b6165b44ee80140b3627e8
tpalinski <tpalinski@icloud.com>
2023-04-16 14:10:18 +0200

    Refactor static html serving, cleanup html parsing

--- src/html_parser.rs
+++ src/html_parser.rs
@@ -1,8 +1,9 @@
-use std::{path::PathBuf, fs};
+use std::fs;
 
 pub fn parse_html(path: &str) -> String {
     let mut absolute_path = std::env::current_dir()
         .expect("Could not get path");
+    absolute_path.push("static");
     absolute_path.push(path);
     let path_string = absolute_path.to_str().expect("Stuff");
     println!("{path_string}");
